c78f29a9eec4b2928a03853fab15db79decf922b
rmullhau <rmullhau@gmail.com>
2018-02-01 15:46:35 -0500

    First Commit of Base Files
    it begins

---
+++ app.js
@@ -0,0 +1,19 @@
+//Requiring Shits
+var express = require('express');
+var app = express();
+var serv = require ('http').Server(app);
+
+//Init Express
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
+serv.listen('2000');
+console.log('Ass blaster 9000 ready for duty on *:2000');
+
+//Init Socket.io
+var io = require('socket.io')(serv, {});
+io.sockets.on('connection', function(socket) {
+  console.log('User connected.');
+  
+});
